8b85ca1216d1dd0ef499c2e96cdc55bfd5416e64
86150 <2414049895@qq.com>
2021-05-25 22:56:53 +0800

    HE Week12 Commit
    Signed-off-by: 86150 <2414049895@qq.com>

--- src/main/java/com/hukangjin/model/Product.java
+++ src/main/java/com/hukangjin/model/Product.java
@@ -3,11 +3,10 @@ package com.hukangjin.model;
 import java.io.InputStream;
 
 public class Product {
-<<<<<<< HEAD
+
     private int productId;
-=======
-    private int productld;
->>>>>>> github.com/master
+
+
     private String productName;
     private String productDescription;
     private InputStream picture;
@@ -16,13 +15,9 @@ public class Product {
 
     public Product(){}
 
-<<<<<<< HEAD
+
     public Product(int productId, String productName, String productDescription, InputStream picture, double price, int categoryId) {
         this.productId = productId;
-=======
-    public Product(int productld, String productName, String productDescription, InputStream picture, double price, int categoryId) {
-        this.productld = productld;
->>>>>>> github.com/master
         this.productName = productName;
         this.productDescription = productDescription;
         this.picture = picture;
@@ -30,57 +25,48 @@ public class Product {
         this.categoryId = categoryId;
     }
 
-<<<<<<< HEAD
     public int getProductId() {
         return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-=======
-    public int getProductld() {
-        return productld;
-    }
-
-    public void setProductld(int productld) {
-        this.productld = productld;
->>>>>>> github.com/master
     }
 
     public String getProductName() {
         return productName;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
     public String getProductDescription() {
         return productDescription;
-    }
-
-    public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
     }
 
     public InputStream getPicture() {
         return picture;
     }
 
-    public void setPicture(InputStream picture) {
-        this.picture = picture;
-    }
-
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public int getCategoryId() {
         return categoryId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public void setPicture(InputStream picture) {
+        this.picture = picture;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public void setCategoryId(int categoryId) {
@@ -90,11 +76,7 @@ public class Product {
     @Override
     public String toString() {
         return "Product{" +
-<<<<<<< HEAD
                 "productId=" + productId +
-=======
-                "productld=" + productld +
->>>>>>> github.com/master
                 ", productName='" + productName + '\'' +
                 ", productDescription='" + productDescription + '\'' +
                 ", picture=" + picture +
